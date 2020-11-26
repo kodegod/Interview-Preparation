6e75396708836de02ecfad01fd961d3d77cf1d22
kodegod <44066441+kodegod@users.noreply.github.com>
2020-11-26 13:27:41 +0530

    Create MaxChunks.cpp

---
+++ Arrays/MaxChunks.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> pmax(n);
+        pmax[0]=arr[0];
+        for(int i=1; i<n; i++)
+            pmax[i]=max(pmax[i-1],arr[i]);
+        int ans=0;
+        for(int i=0; i<n; i++)
+        {
+            if(pmax[i]==i)
+                ans++;
+        }
+        return ans;
+    }
+};
