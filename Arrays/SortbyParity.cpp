58bf18284ce76b74cc4abc9f03e3fb9221902aab
kodegod <44066441+kodegod@users.noreply.github.com>
2021-03-19 20:06:57 +0530

    Create SortbyParity.cpp

---
+++ Arrays/SortbyParity.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/sort-array-by-parity/
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        int n=A.size();
+        int curr=0;
+        int left=0;
+        while(left!=n)
+        {
+            if(A[left]%2==0)
+                swap(A[left],A[curr++]);
+            left++;
+        }
+        return A;
+    }
+};
