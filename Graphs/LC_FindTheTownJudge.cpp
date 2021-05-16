e853d84bacebc78fc6f0d0a9dca1236df3ebd3a6
kodegod <44066441+kodegod@users.noreply.github.com>
2021-05-16 14:35:26 +0530

    Update LC_FindTheTownJudge.cpp

--- Graphs/LC_FindTheTownJudge.cpp
+++ Graphs/LC_FindTheTownJudge.cpp
@@ -1,21 +1,21 @@
 //https://leetcode.com/problems/find-the-town-judge/
 class Solution {
 public:
-    map<int,vector<int>> indegree,outdegree;
+    vector<int> outdegree,indegree;
     int findJudge(int n, vector<vector<int>>& trust) {
+        indegree.resize(n+1,0);
+        outdegree.resize(n+1,0);
         for(int i=0; i<trust.size(); i++)
         {
-            outdegree[trust[i][0]].push_back(trust[i][1]);
-            indegree[trust[i][1]].push_back(trust[i][0]);
+            outdegree[trust[i][0]]++;
+            indegree[trust[i][1]]++;
         }
         int judge=-1;
         for(int i=1; i<=n; i++)
         {
-            if(indegree[i].size()==n-1)
-                judge=i;
+            if(indegree[i]==n-1 && outdegree[i]==0)
+                return i;
         }
-        if(outdegree[judge].size()==0)
-            return judge;
         return -1;
     }
 };
