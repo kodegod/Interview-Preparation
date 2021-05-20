275a2fae79ee29dc644298f5c864977e490469af
kodegod <44066441+kodegod@users.noreply.github.com>
2021-05-20 13:10:13 +0530

    Update LC_AsFromFromLandAsPossible.cpp

--- Graphs/LC_AsFromFromLandAsPossible.cpp
+++ Graphs/LC_AsFromFromLandAsPossible.cpp
@@ -3,9 +3,9 @@ class Solution {
 public:
     vector<vector<int>> dir={{0,1},{0,-1},{1,0},{-1,0}};
     map<pair<int,int>,bool> visited;
-    void bfs(vector<vector<int>>& grid,int&n)
+    int bfs(vector<vector<int>>& grid,int&n)
     {
-        queue<pair<int,int>> q;
+        queue<pair<int,int>> q,q1;
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<n; j++)
@@ -17,47 +17,37 @@ public:
                 }
             }
         }
+        int cnt=0;
         while(!q.empty())
         {
-            pair<int,int> node=q.front();
-            q.pop();
-            for(int i=0; i<4; i++)
+            cnt++;
+            while(!q.empty())
             {
-                int x=node.first+dir[i][0];
-                int y=node.second+dir[i][1];
-                if(x<0 || y<0 || x>=n || y>=n)
-                    continue;
-                if(!visited[{x,y}])
+                pair<int,int> node=q.front();
+                q.pop();
+                for(int i=0; i<4; i++)
                 {
-                    grid[x][y]=1+grid[node.first][node.second];
-                    q.push({x,y});
-                    visited[{x,y}]=true;
+                    int x=node.first+dir[i][0];
+                    int y=node.second+dir[i][1];
+                    if(x<0 || y<0 || x>=n || y>=n)
+                        continue;
+                    if(!visited[{x,y}])
+                    {
+                        grid[x][y]=1+grid[node.first][node.second];
+                        q1.push({x,y});
+                        visited[{x,y}]=true;
+                    }
                 }
             }
+            swap(q,q1);
         }
+        return cnt;
     }
     int maxDistance(vector<vector<int>>& grid) {
         int n=grid.size();
-        bool water=false,land=false;
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(grid[i][j]==0)
-                    water=true;
-                if(grid[i][j]==1)
-                    land=true;
-            }
-        }
-        if(!water || !land)
+        int ans=bfs(grid,n);
+        if(ans==1)
             return -1;
-        bfs(grid,n);
-        int ans=0;
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<n; j++)
-                ans=max(ans,grid[i][j]);
-        }
         return ans-1;
     }
 };
