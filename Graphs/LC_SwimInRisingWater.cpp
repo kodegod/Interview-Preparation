b7eaca39ba2b05bfad3b0296c435684e4d22298a
kodegod <44066441+kodegod@users.noreply.github.com>
2021-05-31 12:50:03 +0530

    Update LC_SwimInRisingWater.cpp

--- Graphs/LC_SwimInRisingWater.cpp
+++ Graphs/LC_SwimInRisingWater.cpp
@@ -33,6 +33,67 @@ public:
 };
 
 
-// MST - Kruskal - DSU
+// MST - Kruskal
+
+class Solution {
+public:
+    vector<int> par;
+    vector<vector<int>> dir={{0,1},{0,-1},{1,0},{-1,0}};
+    int get_root(int u)
+    {
+        if(u==par[u])
+            return u;
+        return par[u]=get_root(par[u]);
+    }
+    void merge(int u,int v){
+        int a=get_root(u);
+        int b=get_root(v);
+        if(a==b)
+            return;
+        if(rand()%2)
+            par[a]=b;
+        else
+            par[b]=a;
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        int n=grid.size();
+        par.resize(n*n,0);
+        for(int i=0; i<par.size(); i++)
+            par[i]=i;
+        vector<pair<int,pair<int,int>>> edgelist;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                for(int k=0; k<4; k++)
+                {
+                    int x=i+dir[k][0];
+                    int y=j+dir[k][1];
+                    if(x<0||y<0||x>=n||y>=n)
+                        continue;
+                    edgelist.push_back({max(grid[x][y],grid[i][j]),{grid[i][j],grid[x][y]}});
+                }
+            }
+        }
+        int ans=0;
+        sort(edgelist.begin(),edgelist.end());
+        for(auto it:edgelist)
+        {
+            int x=get_root(it.second.first);
+            int y=get_root(it.second.second);
+            if(x==y)
+                continue;
+            else
+            {
+                merge(it.second.first,it.second.second);
+                int a=get_root(grid[0][0]);
+                int b=get_root(grid[n-1][n-1]);
+                if(a==b)
+                    return it.first;
+            }
+        }
+        return 0;
+    }
+};
 
 
