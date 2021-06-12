1a3c073f3d73016c96533908e5de1997300a65e0
kodegod <44066441+kodegod@users.noreply.github.com>
2021-06-12 21:24:58 +0530

    Create LC_BulbSwitcher.cpp

---
+++ Brainteaser/LC_BulbSwitcher.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/bulb-switcher/
+
+// IDEA!
+// Perfect squares have odd number of factors always
+// All other non-perfect squares always have even number of factors 
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
