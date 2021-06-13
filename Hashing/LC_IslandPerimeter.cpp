8ce59f63870bccbc80322831d0747f273b5673d6
kodegod <44066441+kodegod@users.noreply.github.com>
2021-06-13 11:42:38 +0530

    Create LC_IslandPerimeter.cpp

---
+++ Hashing/LC_IslandPerimeter.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/island-perimeter/
+
+// Not hashtable
+
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        int ans=0;
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    ans+=4;
+                    if(i-1>=0 && grid[i-1][j]==1)
+                        ans-=2;
+                    if(j-1>=0 && grid[i][j-1]==1)
+                        ans-=2;
+                }
+            }
+        }
+        return ans;
+    }
+};
