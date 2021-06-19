46ac536df849e7b48754c25d8cb774efd57da28c
kodegod <44066441+kodegod@users.noreply.github.com>
2021-06-19 20:02:36 +0530

    Create LC_MinimumWindowSubstring.cpp

---
+++ Hashing/LC_MinimumWindowSubstring.cpp
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/minimum-window-substring/
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        map<char,int> freq_t;
+        map<char,int> freq_s;
+        for(int i=0; i<m; i++)
+            freq_t[t[i]]++;
+        int match_count=0;
+        int ans=INT_MAX;
+        int i=0;
+        int ans_i,ans_j;
+        for(int j=0; j<n; j++)
+        {
+            freq_s[s[j]]++;
+            if(freq_s[s[j]]<=freq_t[s[j]])
+                match_count++;
+            if(match_count==m)
+            {
+                while(match_count==m)
+                {
+                    freq_s[s[i]]--;
+                    if(freq_s[s[i]]<freq_t[s[i]])
+                        match_count--;
+                    i++;
+                }
+                if(j-i+2<ans)
+                {
+                    ans=j-i+2;
+                    ans_i=i-1;
+                    ans_j=j;
+                }
+                ans=min(ans,j-i+2);
+            }
+        }
+        if(ans==INT_MAX)
+            return "";
+        return s.substr(ans_i,ans_j-ans_i+1);
+    }
+};
