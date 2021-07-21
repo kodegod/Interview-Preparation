534b5946be531611edf4b58ea879093616892d15
kodegod <44066441+kodegod@users.noreply.github.com>
2021-07-21 11:24:33 +0530

    Create LC_CoinChange.cpp

---
+++ Dynamic Programming/LC_CoinChange.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/coin-change/
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        int dp[amount+1];
+        dp[0]=0;
+        for(int i=1; i<=amount; i++)
+        {
+            dp[i]=INT_MAX;
+            for(int j=0; j<coins.size(); j++)
+                if(coins[j]<=i && dp[i-coins[j]]!=INT_MAX)
+                    dp[i]=min(dp[i],dp[i-coins[j]]+1);
+        }
+        return (dp[amount]==INT_MAX)?-1:dp[amount];
+    }
+};
