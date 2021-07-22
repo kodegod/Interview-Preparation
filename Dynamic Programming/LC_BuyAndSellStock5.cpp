948011b9e0a030263d47b584fb777099555e900d
kodegod <44066441+kodegod@users.noreply.github.com>
2021-07-22 11:24:17 +0530

    Update LC_BuyAndSellStock5.cpp

--- Dynamic Programming/LC_BuyAndSellStock5.cpp
+++ Dynamic Programming/LC_BuyAndSellStock5.cpp
@@ -13,9 +13,9 @@ public:
         for(int i=1; i<n; i++)
         {
             hasStock[0][i] = max(hasStock[0][i-1], -prices[i]);
-            noStock[0][i] = max(noStock[0][i-1], hasStock[0][i] + prices[i]);
-            hasStock[1][i] = max(hasStock[1][i-1], noStock[0][i] - prices[i]);
-            noStock[1][i] = max(noStock[1][i-1], hasStock[1][i] + prices[i]);
+            noStock[0][i] = max(noStock[0][i-1], hasStock[0][i-1] + prices[i]);
+            hasStock[1][i] = max(hasStock[1][i-1], noStock[0][i-1] - prices[i]);
+            noStock[1][i] = max(noStock[1][i-1], hasStock[1][i-1] + prices[i]);
         }
         return noStock[1][n-1];
     }
