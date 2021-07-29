5dbf285453f37f55270d20e2cd7468a7145bb900
kodegod <44066441+kodegod@users.noreply.github.com>
2021-07-29 20:23:52 +0530

    Update CS_MatrixChainMultiplication.cpp

--- Dynamic Programming/CS_MatrixChainMultiplication.cpp
+++ Dynamic Programming/CS_MatrixChainMultiplication.cpp
@@ -1,5 +1,8 @@
 //https://www.codingninjas.com/codestudio/problems/matrix-chain-multiplication_975344?leftPanelTab=1
 
+// MCM
+
+//Recursive
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -30,4 +33,28 @@ int matrixMultiplication(vector<int> &arr, int N)
     memset(dp, -1, sizeof dp);
     return solve(arr,N,0,N-2);
     // Write your code here.
+}
+
+// Iterative
+
+int matrixMultiplication(vector<int> &arr, int N)
+{
+    // Write your code here.
+    int M = N-1; // Number of matrices
+    int dp[N-1][N-1];
+    for(int i=0; i<N-1; i++)
+        dp[i][i] = 0;
+    for(int d=1; d<N-1; d++)
+    {
+        int i = 0, j = d;
+        while(i<N-1 && j<N-1)
+        {
+            dp[i][j] = INT_MAX;
+            for(int k=i; k<j; k++)
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + arr[i]*arr[k+1]*arr[j+1]);
+            i++;
+            j++;
+        }
+    }
+    return dp[0][N-2];
 }
