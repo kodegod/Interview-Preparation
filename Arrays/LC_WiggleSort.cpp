39952b78487ea699cf3c2b33a5e03b2249c24e14
kodegod <44066441+kodegod@users.noreply.github.com>
2021-08-08 10:53:20 +0530

    Create LC_WiggleSort.cpp

---
+++ Arrays/LC_WiggleSort.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/wiggle-sort/
+
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1)
+            return;
+        if(n==2)
+        {
+            if(nums[0]>nums[1])
+                swap(nums[0],nums[1]);
+            return;
+        }
+        for(int i=1; i<n; i+=2)
+        {
+            if(i==n-1)
+            {
+                if(nums[i]<nums[i-1])
+                    swap(nums[i],nums[i-1]);
+                continue;
+            }
+            if(nums[i]>=nums[i-1] && nums[i]>=nums[i+1])
+                continue;
+            else if(nums[i-1]>nums[i+1])
+                swap(nums[i-1],nums[i]);
+            else
+                swap(nums[i+1],nums[i]);
+        }
+    }
+};
