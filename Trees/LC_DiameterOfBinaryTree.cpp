ff22627e103ccc781effe5e866bba113fad4e26e
kodegod <44066441+kodegod@users.noreply.github.com>
2021-09-30 01:34:19 +0530

    Update LC_DiameterOfBinaryTree.cpp

--- Trees/LC_DiameterOfBinaryTree.cpp
+++ Trees/LC_DiameterOfBinaryTree.cpp
@@ -2,22 +2,18 @@
 
 class Solution {
 public:
-    int ans = -1;
+    int ans = 0;
     int dia(TreeNode* root)
     {
-        if(root->left==NULL && root->right==NULL)
+        if(root==NULL)
             return 0;
-        int d1=-1;
-        int d2=-1;
-        if(root->left!=NULL)
-            d1=dia(root->left);
-        if(root->right!=NULL)
-            d2=dia(root->right);
-        ans=max(ans,1+d1+d2);
+        int d1=dia(root->left);
+        int d2=dia(root->right);
+        ans=max(ans,max(1+d1+d2,max(d1,d2)));
         return 1+max(d1,d2);
     }
     int diameterOfBinaryTree(TreeNode* root) {
         int x = dia(root);
-        return ans+1;
+        return ans-1;
     }
 };
